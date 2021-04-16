7cfbaac0b7c5902e6ae22c0eb416e2748d8ded69
Raphael Nestler <raphael.nestler@gmail.com>
2021-04-16 16:19:41 +0200

    Add a v14 deserialization example
    This is a minimal v14 compatible struct. It currently fails to
    deserialize, since we still require some now optional fields.

--- examples/deserialization.rs
+++ examples/deserialization.rs
@@ -2,8 +2,15 @@ use serde_json::from_str;
 use spaceapi::Status;
 
 fn main() {
-    let input = r#"{"api":"0.13","space":"coredump","logo":"https://www.coredump.ch/logo.png","url":"https://www.coredump.ch/","location":{"lat":47.22936,"lon":8.82949},"contact":{"irc":"irc://freenode.net/#coredump","twitter":"@coredump_ch","foursquare":"525c20e5498e875d8231b1e5","email":"danilo@coredump.ch"},"issue_report_channels":["email","twitter"],"state":{"open":null},"ext_ccc":"chaostreff"}"#;
-    let status: Result<Status, _> = from_str(input);
+    let v13_input = r#"{"api":"0.13","space":"coredump","logo":"https://www.coredump.ch/logo.png","url":"https://www.coredump.ch/","location":{"lat":47.22936,"lon":8.82949},"contact":{"irc":"irc://freenode.net/#coredump","twitter":"@coredump_ch","foursquare":"525c20e5498e875d8231b1e5","email":"danilo@coredump.ch"},"issue_report_channels":["email","twitter"],"state":{"open":null},"ext_ccc":"chaostreff"}"#;
+    let status: Result<Status, _> = from_str(v13_input);
+    match status {
+        Ok(status) => println!("{:?}", status),
+        Err(err) => println!("Could not parse status: {}", err),
+    };
+
+    let v14_input = r#"{"api_compatibility":["14"],"space":"coredump","logo":"https://www.coredump.ch/logo.png","url":"https://www.coredump.ch/","location":{"lat":47.22936,"lon":8.82949},"contact":{"irc":"irc://freenode.net/#coredump","twitter":"@coredump_ch","foursquare":"525c20e5498e875d8231b1e5","email":"danilo@coredump.ch"},"ext_ccc":"chaostreff"}"#;
+    let status: Result<Status, _> = from_str(v14_input);
     match status {
         Ok(status) => println!("{:?}", status),
         Err(err) => println!("Could not parse status: {}", err),
