4611401345fcc2c09113c13cbc10aec4d92c676b
Raphael Nestler <raphael.nestler@gmail.com>
2024-05-05 21:44:40 +0200

    Add linked_spaces field

--- src/status.rs
+++ src/status.rs
@@ -225,6 +225,12 @@ pub enum ApiVersion {
     V14,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
+pub struct LinkedSpace {
+    endpoint: String,
+    website: String,
+}
+
 /// The main SpaceAPI status object.
 #[derive(Serialize, Deserialize, Default, Debug, Clone, PartialEq)]
 pub struct Status {
@@ -260,6 +266,8 @@ pub struct Status {
     pub links: Option<Vec<Link>>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub membership_plans: Option<Vec<MembershipPlan>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub linked_spaces: Option<Vec<LinkedSpace>>,
 
     // SpaceAPI internal usage
     #[serde(skip_serializing_if = "Option::is_none")]
